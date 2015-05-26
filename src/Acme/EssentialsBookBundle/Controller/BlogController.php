f45814ec772190a173f1550c6360e8345e64dcd3
Yehor Konoval <ekonoval@gmail.com>
2015-05-26 11:25:49 +0300

    blog in annotations

--- src/Acme/EssentialsBookBundle/Controller/BlogController.php
+++ src/Acme/EssentialsBookBundle/Controller/BlogController.php
@@ -3,15 +3,41 @@ namespace Acme\EssentialsBookBundle\Controller;
 
 use Acme\EssentialsBookBundle\Helpers\BundleController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
 
 class BlogController extends BundleController
 {
+//    /**
+//     * @Route("/blog/{slug}", name="blog_show")
+//     */
+//    public function showAction($slug)
+//    {
+//        pa($slug);
+//    }
+
     /**
-     * @Route("/blog/{slug}", name="blog_show")
+     * @Route("/blog/{page}", defaults={"page": 1}, requirements={"page": "\d+" })
      */
-    public function showAction($slug)
+    public function indexAction($page)
     {
-        pa($slug);
+
+        pa('page', $page);
     }
 
+    /**
+     * @Route("/blog/{slug}", defaults={"slug": 122}, name="acme_esb_blog_slug")
+     */
+    public function blogSlugAction($slug)
+    {
+        /** @var Router $router */
+        $router = $this->get('router');
+        $params = $router->match('/blog/my-blog-post');
+        pa($params);
+
+        $url = $router->generate('acme_esb_blog_slug', ['slug' => 'my-fake- slu\g']);
+        pa($url);
+
+        pa('slug', $slug);
+        pa($_route);
+    }
 }
