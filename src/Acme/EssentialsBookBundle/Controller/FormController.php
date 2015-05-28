9ad10c86838938b7cc7eb1141c4590d8a996b279
Yehor Konoval <ekonoval@gmail.com>
2015-05-28 15:35:40 +0300

    basic form

--- src/Acme/EssentialsBookBundle/Controller/FormController.php
+++ src/Acme/EssentialsBookBundle/Controller/FormController.php
@@ -1,9 +1,11 @@
 <?php
 namespace Acme\EssentialsBookBundle\Controller;
 
+use Acme\EssentialsBookBundle\Entity\Form\Task;
 use Acme\EssentialsBookBundle\Helpers\BundleController;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class FormController extends BundleController
 {
@@ -12,6 +14,40 @@ class FormController extends BundleController
      */
     public function indexAction()
     {
-        pa(); exit;
+        pa();
+        exit;
+    }
+
+    /**
+     * @Route("/new", name="ebb_form_new")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function newAction(Request $request)
+    {
+        // create a task and give it some dummy data for this example
+        $task = new Task();
+        $task->setTask('Write a blog post');
+        $task->setDueDate(new \DateTime('tomorrow'));
+
+        $form = $this->createFormBuilder($task)
+            ->add('task', 'text')
+            ->add('dueDate', 'date')
+            ->add('save', 'submit', array('label' => 'Create Task'))
+            ->add('saveAndAdd', 'submit', array('label' => 'Save and Add'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+
+            $form->get('saveAndAdd')->isClicked();
+
+            return $this->redirectToRoute('ebb_form_new', ['done' => time()]);
+        }
+
+        return $this->renderAuto('Form', 'new.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
