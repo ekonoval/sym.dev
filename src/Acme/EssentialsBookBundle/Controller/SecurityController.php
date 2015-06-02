7463d0a8d3203b252746dea41391ec99bc1efee3
Yehor Konoval <ekonoval@gmail.com>
2015-06-02 14:11:54 +0300

    security init

---
+++ src/Acme/EssentialsBookBundle/Controller/SecurityController.php
@@ -0,0 +1,16 @@
+<?php
+namespace Acme\EssentialsBookBundle\Controller;
+
+use Acme\EssentialsBookBundle\Helpers\BundleController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class SecurityController extends BundleController
+{
+    /**
+     * @Route("/admin")
+     */
+    public function adminAction()
+    {
+
+    }
+}
