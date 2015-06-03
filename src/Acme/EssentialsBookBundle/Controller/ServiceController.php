1a53461660a31e448cad153158c096de02a209d9
Yehor Konoval <ekonoval@gmail.com>
2015-06-03 14:06:36 +0300

    serv parameters

--- src/Acme/EssentialsBookBundle/Controller/ServiceController.php
+++ src/Acme/EssentialsBookBundle/Controller/ServiceController.php
@@ -1,7 +1,9 @@
 <?php
 namespace Acme\EssentialsBookBundle\Controller;
 
+use Acme\EssentialsBookBundle\Helpers\BookMailer;
 use Acme\EssentialsBookBundle\Helpers\BundleController;
+use Acme\EssentialsBookBundle\Helpers\PlainResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class ServiceController extends BundleController
@@ -11,6 +13,15 @@ class ServiceController extends BundleController
      */
     public function indexAction()
     {
-        pa(); exit;
+        $this->_t1();
+        return new PlainResponse();
+    }
+
+    private function _t1()
+    {
+        /** @var BookMailer $mailer */
+        $mailer = $this->get('my_mailer');
+
+        $mailer1 = $this->get('my_mailer');
     }
 }
